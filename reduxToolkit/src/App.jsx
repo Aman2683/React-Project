496f13de7d2be18f73a8bff0acd27e0091d34644
Aman Kumar <ak872259@gmail.com>
2024-02-23 14:22:35 +0530

    Finalizing

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='flex justify-center p-4'> Redux Toolkit </h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
